b7bbaea71410ae77ead5cdb5b993376e136b3e62
cuzknothz <91480011+cuzknothz@users.noreply.github.com>
2023-03-27 23:46:48 +0700

    🥕

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
-import * as THREE from "three";
-import { Inter } from "next/font/google";
+import { Canvas } from "@react-three/fiber";
 import { getProject } from "@theatre/core";
+import extension from "@theatre/r3f/dist/extension";
 import studio from "@theatre/studio";
+import { Inter } from "next/font/google";
+import { SheetProvider, editable as e, PerspectiveCamera } from "@theatre/r3f";
 
 studio.initialize();
+studio.extend(extension);
+
 const inter = Inter({ subsets: ["latin"] });
 
 const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
@@ -15,17 +18,26 @@ export default function Home() {
   return (
     <Canvas
       className="[&__canvas]:!w-screen [&__canvas]:!h-screen"
-      camera={{
-        position: [5, 5, -5],
-        fov: 75,
-      }}
+      gl={{ preserveDrawingBuffer: true }}
     >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <SheetProvider sheet={demoSheet}>
+        <PerspectiveCamera
+          theatreKey="Camera"
+          makeDefault
+          position={[5, 5, -5]}
+          fov={75}
+          attachArray={undefined}
+          attachObject={undefined}
+          attachFns={undefined}
+        />
+
+        <ambientLight />
+        <e.pointLight position={[10, 10, 10]} theatreKey="pointLight" />
+        <e.mesh theatreKey="cube">
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </e.mesh>
+      </SheetProvider>
     </Canvas>
   );
 }
